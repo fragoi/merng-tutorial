c5d37284e87b31be547d1a383c9e7d6e6c7e7f39
Fra <francesco.goinavi@gmail.com>
2021-01-19 14:30:37 +0100

    Navigation bar and pages draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Container>
+      <BrowserRouter>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/signin' component={Signin} />
+        <Route exact path='/signup' component={Signup} />
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default function MenuBar() {
+  const pathname = window.location.pathname;
+  const name = pathname === '/' ? 'home' : pathname.substr(1);
+
+  const [activeItem, setActiveItem] = useState(name);
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary size='massive' color='teal'>
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to='/'
+      />
+      <Menu.Menu position='right'>
+        <Menu.Item
+          name='signup'
+          active={activeItem === 'signup'}
+          onClick={handleItemClick}
+          as={Link}
+          to='/signup'
+        />
+        <Menu.Item
+          name='signin'
+          active={activeItem === 'signin'}
+          onClick={handleItemClick}
+          as={Link}
+          to='/signin'
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
